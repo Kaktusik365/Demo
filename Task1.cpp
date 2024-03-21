0a5729c2d465412b88e979da52103cec746a6d70
Яфошкина Елена Александровна ТСА-141 <160817774+Kaktusik365@users.noreply.github.com>
2024-03-21 14:06:26 +0300

    Add files via upload

--- Task1.cpp
+++ Task1.cpp
@@ -1,41 +1,51 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
 /**
  * @brief Функция для вычисления значения переменной a по заданным константам.
+ * @param x значение константы .
+ * @param y значение константы .
+ * @param z значение константы .
  * @return Результат вычисления переменной a.
 */
+double calculateA(const double x, const double y, double z)
 
-double calculateA(double x, double y, double z)
-{
-    return pow(sin(pow(x, 2) + z), 3) - sqrt(x / y);
-}
 
-/*
+/**
  * @brief Функция для вычисления значения переменной b по заданным константам.
+ * @param x значение константы .
+ * @param z значение константы .
  * @return Результат вычисления переменной b.
 */
+double calculateB(const double x, const double z)
 
-double calculateB(double x, double z)
-{
-    return pow(x, 2) / z + pow(cos(x), 3);
-}
-
+/**
+ * @brief главная функция программы
+ * @return возвращает 0 правильно, в противном случае - 0
+ */
 int main()
 {
-   const double x = 0.2;
-   const double y = 0.004;
-   const double z = 1.1;
-
-    double a = calculateA(x, y, z);
-    double b = calculateB(x, z);
+    const double x = 0.2;
+    const double y = 0.004;
+    const double z = 1.1;
 
     cout << "x = " << x << endl;
     cout << "y = " << y << endl;
     cout << "z = " << z << endl;
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
+    cout << "a = " << calculateA(x, y, z) << endl;
+    cout << "b = " << calculateB(x, z) << endl;
 
     return 0;
+}
+
+
+double calculateA(const double x, const double y, double z)
+{
+    return pow(sin(pow(x, 2) + z), 3) - sqrt(x / y);
+}
+
+double calculateB(const double x, const double z)
+{
+    return pow(x, 2) / z + pow(cos(x), 3);
 }
