70a2c50e98fe456b0846b1a4337a99d45297b32b
Яфошкина Елена Александровна ТСА-141 <160817774+Kaktusik365@users.noreply.github.com>
2024-05-27 19:45:51 +0300

    Add files via upload

--- task 4.cpp
+++ task 4.cpp
@@ -1,33 +1,34 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
 #include <limits>
 using namespace std;
+
 /**
-*@brief Считывает значиния с клавиатуры с проверкой ввода
-*@return возвращает значение, если оно правильное, иначе завершает программу
-*/
+ * @brief Считывает значение с клавиатуры с проверкой ввода
+ * @return Возвращает значение, если оно правильное, иначе завершает программу
+ */
 double getValue();
 
 /**
-*@brief  Функция для вычисления значения функции y = tan(X) - (1/3*(pow(tan(x), 3))) + (1/5*(pow(tan(x), 5)))-1/3
-*@return tan(X) - (1/3*(pow(tan(x), 3))) + (1/5*(pow(tan(x), 5)))-1/3
-*/
+ * @brief Функция для вычисления значения функции y = tan(x) - 1/3*tan^3(x) + 1/5*tan^5(x) - 1/3
+ * @param x Входное значение x
+ * @return Значение y = tan(x) - 1/3*tan^3(x) + 1/5*tan^5(x) - 1/3
+ */
 double calculateFunction(double x);
 
 /**
-*@brief  Функция для проверки, что шаг положительный
-*@param step шаг с которым идёт постороение графика
-*@return step
-*/
-double getStep();
-
+ * @brief Функция для проверки, что шаг положительный
+ * @return Положительный шаг
+ */
+double getPositiveStep();
 
 /**
-*@brief  точка хода в программу
-*@return 0
-*/
+ * @brief Точка входа в программу
+ * @return 0
+ */
 int main()
 {
+    setlocale(LC_ALL, "Russian");
 
     cout << "Введите начальное значение x: ";
     double startX = getValue();
@@ -41,15 +42,26 @@ int main()
         return 1;
     }
 
-    cout << "Введите шаг: ";
-    double step = getValue();
-    double getStep();
+    double step = getPositiveStep();
 
+    cout << "x | y" << endl;
+    cout << "--------" << endl;
+
+    for (double x = startX; x < endX; x += step)
+    {
+        if (fabs(tan(x) - 1.0 / 3.0 * pow(tan(x), 3) + 1.0 / 5.0 * pow(tan(x), 5) - 1.0 / 3) < numeric_limits<double>::epsilon())
+        {
+            cout << "Решение невозможно для x = " << x << endl;
+        }
+        else
+        {
+            double y = calculateFunction(x);
+            cout << x << " | " << y << endl;
+        }
+    }
 
     return 0;
 }
-
-
 
 double getValue()
 {
@@ -63,21 +75,18 @@ double getValue()
     return value;
 }
 
-
-
 double calculateFunction(double x)
 {
-    return tan(X) - (1/3*(pow(tan(x), 3))) + (1/5*(pow(tan(x), 5)))-1/3;
+    return tan(x) - 1.0 / 3.0 * pow(tan(x), 3) + 1.0 / 5.0 * pow(tan(x), 5) - 1.0 / 3;
 }
 
-
-double getStep()
+double getPositiveStep()
 {
     double step;
     do {
         cout << "Введите шаг: ";
         step = getValue();
-        if (step <= 0) 
+        if (step <= 0)
         {
             cout << "Ошибка. Шаг должен быть положительным. Повторите ввод." << endl;
         }
